7f2e837891946dc863311acc72e237208ea2ec4a
Andrew Y Han <andrew@techsamurais.com>
2020-07-15 20:32:52 -0700

    added getMessages method that grabs all messages in channel

--- index.js
+++ index.js
@@ -6,4 +6,39 @@ client.on('ready', () => {
   console.info(`Logged in as ${client.user.tag}!`)
 })
 
+const getMessages = async (channel) => {
+  const messagesArray = []
+  let lastId = null
+  let isLoop = true
+  while (isLoop) {
+    await channel.messages.fetch({ limit: 100, before: lastId }).then(async msg => {
+      try {
+        for (const msgItem of msg.array()) {
+          messagesArray.push(msgItem.content)
+        }
+        lastId = msg.last().id
+        console.log({ msgSize: msg.size })
+        if (msg.size != 100) {
+          isLoop = false
+        }
+      }
+      catch (error) {
+        console.log('error ---', error)
+      }
+    })
+  }
+  console.log({
+    messagesArray,
+    lastId,
+    length: messagesArray.length
+  })
+  return messagesArray
+}
+
+client.on('message', message => {
+  if (message.content === '/message') {
+    getMessages(message.channel)
+  }
+})
+
 client.login(process.env.TOKEN)
